a72f7290abbcae0fc43cae4526e1df98c12d829b
luigie <sanchezluigie@gmail.com>
2026-09-03 21:45:27 +0800

    --3

--- app.py
+++ app.py
@@ -1,66 +1,13 @@
-full_name = "Luigie Glinofria Sanchez"
-year = 3
-section = "BSCS 3A"
-distance = 6.5
-working_student = True
+decimal = int(input("Enter a decimal number (0-255): "))
 
-subjects = ["Computer Architecture and Organization", "Networking and Communication", "Machine Learning"]
+if 0 <= decimal <= 300:
+    binary = format(decimal, "08b")
+    octal = format(decimal, "o")
+    hexadecimal = format(decimal, "X")
 
-print("--- STUDENT INFORMATION ---")
-print("Full Name:", full_name)
-print("Year Level:", year)
-print("Section:", section)
-print("Distance from Home to School:", distance, "km")
-print("Working Student:", working_student)
-print("Subjects:", subjects)
-
-print("\n--- IF-ELSE ---")
-
-if working_student:
-    print(full_name, "is a working student.")
+    print("\nConverted Number:")
+    print("Binary:      ", binary)
+    print("Octal:       ", octal)
+    print("Hexadecimal: ", hexadecimal)
 else:
-    print(full_name, "is not a working student.")
-
-print("\n--- FOR LOOP ---")
-
-for subject in subjects:
-    print("Subject:", subject)
-
-print("\n--- WHILE LOOP ---")
-
-current_year = 1
-
-while current_year <= year:
-    print("Completed/Current Year:", current_year)
-    current_year += 1
-
-
-print("\n--- MEMORY SIMULATION ---")
-
-memory = [
-    full_name,
-    year,
-    section,
-    distance,
-    working_student,
-    subjects
-]
-
-print("Student information stored in memory:")
-
-for index in range(len(memory)):
-    print("Memory Location", index, ":", memory[index])
-
-print("\nReading specific memory locations:")
-print("Memory[0] - Full Name:", memory[0])
-print("Memory[1] - Year:", memory[1])
-print("Memory[2] - Section:", memory[2])
-print("Memory[3] - Distance:", memory[3])
-print("Memory[4] - Working Student:", memory[4])
-print("Memory[5] - Subjects:", memory[5])
-
-
-print("\n--- CPU AND MEMORY INTERACTION ---")
-print("The CPU requests student data from memory.")
-print("Memory provides the requested data to the CPU.")
-print("CPU processes the data and displays the result.")
+    print("Error: Please enter a number between 0 and 255.")
